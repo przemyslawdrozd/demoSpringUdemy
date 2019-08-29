23b0311f4d2fbdc5e13e2e906d1a1df6058f4f52
przemo06133 <przemo06133@gmail.com>
2019-08-29 18:13:16 +0200

    Exception added

---
+++ src/main/java/com/przemo/demo/exceptions/ApiRequestException.java
@@ -0,0 +1,12 @@
+package com.przemo.demo.exceptions;
+
+public class ApiRequestException extends RuntimeException {
+
+    public ApiRequestException(String message) {
+        super(message);
+    }
+
+    public ApiRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
